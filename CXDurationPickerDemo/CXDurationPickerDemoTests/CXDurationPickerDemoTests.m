df122bf9e07946116abe838c3aaab9c86a8d62b0
Richard Puckett <richard.puckett@concur.com>
2015-05-05 15:33:59 -0700

    Added straw man test.

--- CXDurationPickerDemo/CXDurationPickerDemoTests/CXDurationPickerDemoTests.m
+++ CXDurationPickerDemo/CXDurationPickerDemoTests/CXDurationPickerDemoTests.m
@@ -1,11 +1,3 @@
-//
-//  CXDurationPickerDemoTests.m
-//  CXDurationPickerDemoTests
-//
-//  Created by Richard Puckett on 3/8/15.
-//  Copyright (c) 2015 Concur Labs. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
@@ -17,24 +9,23 @@
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
 - (void)testExample {
-    // This is an example of a functional test case.
     XCTAssert(YES, @"Pass");
 }
 
+/*
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
     }];
 }
+*/
 
 @end
