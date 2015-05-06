a95d0f580c2c0869dd4bc269d82117f4dc6670c0
Richard Puckett <richard.puckett@concur.com>
2015-05-06 13:10:27 -0700

    Additional unit test.

--- CXDurationPickerDemo/CXDurationPickerDemoTests/CXDurationPickerDemoTests.m
+++ CXDurationPickerDemo/CXDurationPickerDemoTests/CXDurationPickerDemoTests.m
@@ -1,6 +1,9 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
+#import "CXDurationPickerDate.h"
+#import "CXDurationPickerUtils.h"
+
 @interface CXDurationPickerDemoTests : XCTestCase
 
 @end
@@ -17,6 +20,19 @@
 
 - (void)testExample {
     XCTAssert(YES, @"Pass");
+}
+
+- (void)testUtilComponents {
+    CXDurationPickerDate pickerDate;
+    pickerDate.day = 1;
+    pickerDate.month = 1;
+    pickerDate.year = 2015;
+    
+    NSDateComponents *testComponents = [CXDurationPickerUtils dateComponentsFromPickerDate:pickerDate];
+    
+    XCTAssertEqual(testComponents.day, 1, @"Incorrect day returned.");
+    XCTAssertEqual(testComponents.month, 1, @"Incorrect month returned.");
+    XCTAssertEqual(testComponents.year, 2015, @"Incorrect year returned.");
 }
 
 /*
